3e878741b1dfabb90b136091ed9a9820da94e38b
David (DongYun)  Kim <pander0616@hotmail.com>
2025-03-05 21:55:42 -0600

    change it to localhost again

--- ui/app/api/apiClient.ts
+++ ui/app/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://172.16.1.63:5001";
+const API_BASE_URL = "http://localhost:5001";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
