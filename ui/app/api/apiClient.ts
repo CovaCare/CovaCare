9330047fb960bb2b31e8390ad1f10112a5585d59
David (DongYun)  Kim <pander0616@hotmail.com>
2025-03-08 00:26:32 -0600

    Revert API base URL to localhost

--- ui/app/api/apiClient.ts
+++ ui/app/api/apiClient.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const API_BASE_URL = "http://localhost:5001";
-=======
-const API_BASE_URL = "https://172.16.1.63:5001";
->>>>>>> origin/main
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
