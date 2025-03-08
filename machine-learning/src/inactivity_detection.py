5cdb8e03c33d590794ec3f77b7439b746c35c363
Brydon <brydon2002@hotmail.com>
2025-03-08 16:25:46 -0600

    Small inactivity detection bug

--- machine-learning/src/inactivity_detection.py
+++ machine-learning/src/inactivity_detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from config import MP_LANDMARK_SIZE
 
 class InactivityMonitor:
     def __init__(self, sensitivity, duration):
@@ -10,7 +11,7 @@ class InactivityMonitor:
         self.last_frame_time = time.time()
 
     def check_inactivity(self, current_keypoints):
-        if self.previous_keypoints is not None:
+        if self.previous_keypoints is not None and not np.all(current_keypoints == np.zeros((MP_LANDMARK_SIZE, 2))):
             diff = np.linalg.norm(current_keypoints - self.previous_keypoints, axis=1)
             movement = np.mean(diff)
 
