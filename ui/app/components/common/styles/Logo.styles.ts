83b4517d1317987379917fa43ee886dab215e2d5
David (DongYun)  Kim <pander0616@hotmail.com>
2025-03-26 20:45:07 -0600

    Adjusted container padding and alignment

--- ui/app/components/common/styles/Logo.styles.ts
+++ ui/app/components/common/styles/Logo.styles.ts
@@ -2,8 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    paddingLeft: 15,
-    paddingTop: 17,
+    justifyContent: "flex-end",
+    paddingTop: 19,
+    paddingBottom: 1,
   },
   logo: {
     width: 90,
